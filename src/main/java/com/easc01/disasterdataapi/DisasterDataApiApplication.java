b46791c82c7c45fb4e4986c9b43bec9f3061a509
ishantSikdar <ishant9715@gmail.com>
2024-08-10 18:14:12 +0530

    first commit

---
+++ src/main/java/com/easc01/disasterdataapi/DisasterDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.easc01.disasterdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasterDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easc01/disasterdataapi/DisasterDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easc01.disasterdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
